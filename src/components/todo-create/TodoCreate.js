18ae05976f6ef96a4e76220146b1697fdb65e9f7
lianabdiansyah <baimtjg21@gmail.com>
2021-10-04 21:20:11 +0700

    menambahkan komentar

--- src/components/todo-create/TodoCreate.js
+++ src/components/todo-create/TodoCreate.js
@@ -6,26 +6,27 @@ function TodoCreate(props) {
     const [newInputTodo, setNewInputTodo] = useState("")
 
     const handleSubmit = (event) => {
-        event.preventDefault()
-        const newOneTodo = {
-            id: Math.floor(Math.random() * 100 + 1),
-            title: newInputTodo
-        }
+      event.preventDefault();
+      const newOneTodo = {
+        id: Math.floor(Math.random() * 100 + 1),
+        title: newInputTodo,
+      };
 
-        props.onCreateTodo(newOneTodo)
-        setNewInputTodo('') //akan membuat form kosong
-        // console.log(newOneTodo)
+      props.onCreateTodo(newOneTodo); //newOneTodo parameter yang akan dikirimkan
+      setNewInputTodo(""); //akan membuat form kosong
+      // console.log(newOneTodo)
     }
 
     const handleInputTodo = (event) => {
-        setNewInputTodo(event.target.value)
+        setNewInputTodo(event.target.value) //tanpa value bind dituliskan di form
 
         console.log(newInputTodo)
     }
     return (
         <div>
             <form onSubmit={handleSubmit} className="todo-form">
-                <input type="text" value={newInputTodo} onChange={handleInputTodo}  />
+                <input type="text" value={newInputTodo} onChange={handleInputTodo} />
+                {/* value={newInputTodo} akan selalu mengikuti nilai update input */}
                 <button type="submit">Add</button>
             </form>
         </div>
