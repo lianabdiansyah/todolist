7793fe9cad91cd76952b32517bffd50fef04b4ee
lianabdiansyah <baimtjg21@gmail.com>
2021-10-04 19:54:14 +0700

    membuat komponen todo dan todolist

---
+++ src/todo-list/TodoList.js
@@ -0,0 +1,15 @@
+import './TodoList.css';
+
+function TodoList() {
+    return (
+        <div>
+            <ul>
+                <li>Learn react</li>
+                <li>Build App</li>
+                <li>Deploy to server</li>
+            </ul>
+        </div>
+    )
+}
+
+export default TodoList
