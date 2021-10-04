72c8cd1336d8cc9307823d944caf5700903a4d4b
lianabdiansyah <baimtjg21@gmail.com>
2021-10-04 21:14:19 +0700

    mengosongkan tulisan pada form

--- src/components/todo-create/TodoCreate.js
+++ src/components/todo-create/TodoCreate.js
@@ -1,22 +1,31 @@
+import { useState } from 'react';
 import './TodoCreate.css';
 
 function TodoCreate(props) {
+
+    const [newInputTodo, setNewInputTodo] = useState("")
 
     const handleSubmit = (event) => {
         event.preventDefault()
         const newOneTodo = {
             id: Math.floor(Math.random() * 100 + 1),
-            title: 'read book'
+            title: newInputTodo
         }
 
         props.onCreateTodo(newOneTodo)
+        setNewInputTodo('') //akan membuat form kosong
+        // console.log(newOneTodo)
+    }
 
-        console.log(newOneTodo)
+    const handleInputTodo = (event) => {
+        setNewInputTodo(event.target.value)
+
+        console.log(newInputTodo)
     }
     return (
         <div>
             <form onSubmit={handleSubmit} className="todo-form">
-                <input type="text" />
+                <input type="text" value={newInputTodo} onChange={handleInputTodo}  />
                 <button type="submit">Add</button>
             </form>
         </div>
